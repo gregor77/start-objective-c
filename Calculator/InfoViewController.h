37bb133830cac617a2b4c4594374df86a4eb7958
dwayne park <gregor77@naver.com>
2016-06-16 16:05:53 +0900

    Seg를 통한 화면전환

---
+++ Calculator/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  Calculator
+//
+//  Created by Dwayne Park on 2016. 6. 16..
+//  Copyright © 2016년 Dwayne Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
